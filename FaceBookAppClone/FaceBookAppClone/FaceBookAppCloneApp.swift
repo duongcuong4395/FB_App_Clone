1b958f75e332abe09299d48147556f44619dd606
duongcuong4395 <dangduongcuog@gmail.com>
2025-02-27 16:34:52 +0700

    init

---
+++ FaceBookAppClone/FaceBookAppClone/FaceBookAppCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  FaceBookAppCloneApp.swift
+//  FaceBookAppClone
+//
+//  Created by Macbook on 27/2/25.
+//
+
+import SwiftUI
+
+@main
+struct FaceBookAppCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
